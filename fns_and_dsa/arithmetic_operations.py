3379d40c11c27ed1742ffd2b108ebbf0151015fb
Mercy-Mbaire <mercymbaireh@gmail.com>
2025-11-23 23:28:45 +0300

    Updated the correct version that will pass the checkers

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,17 +1,6 @@
 # arithmetic_operations.py
 
-def perform_operation(num1: float, num2: float, operation: str):
-    """
-    Performs basic arithmetic operations: add, subtract, multiply, divide.
-    
-    Parameters:
-        num1 (float): First number
-        num2 (float): Second number
-        operation (str): 'add', 'subtract', 'multiply', or 'divide'
-    
-    Returns:
-        float or str: Result of the operation or an error message for division by zero or invalid operation
-    """
+def perform_operation(num1, num2, operation):
     if operation == "add":
         return num1 + num2
     elif operation == "subtract":
